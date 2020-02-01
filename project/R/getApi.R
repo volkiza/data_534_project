fab352cf8f86966e56237f3826c11c80475d27df
ShahAlvi <54693077+ShahAlvi@users.noreply.github.com>
2020-01-31 16:30:47 -0800

    Update getApi.R

--- project/R/getApi.R
+++ project/R/getApi.R
@@ -1,44 +1,10 @@
-install.packages("httr")
-install.packages("jsonlite")
+#install.packages("httr")
+#install.packages("jsonlite")
 library(dplyr)
 library(httr)
 library(jsonlite)
 
-makeQuery <- function(classifier) {
-  this.query <- list(classifier)
-  names(this.query) <- "dc"
-  return(this.query)
-}
-
-retreival<-function(params){
-  queries <- lapply(as.list(params), makeQuery)
-  url = 
-  this.raw.result <- GET(url = url, path = path, query = queries[[1]])
-  this.result <- fromJSON(rawToChar(this.raw.result$content))
-  return(this.result)
-}
-
-
-creds<-function(client_id, client_secret){
-  response <- httr::POST(url = "https://api.lufthansa.com/v1/oauth/token",
-                         body=list(client_id = client_id,
-                                   client_secret = client_secret,
-                                   grant_type ="client_credentials"),
-                         encode = "form")
-  response <- httr::content(response, "parsed")
-  
-  token <- response$access_token
-  expires_in <- response$expires_in
-  
-  # Check if there is no token
-  if (is.null(token)) {
-    stop("No token returned. Check credentials.")
-  }
-  
-  creds <- list(token=token)
-  return(creds)
-}
-Authorization <- function(t="ak585uahaecvg4uq3nyqjnsz"){
+Authorization <- function(t="zbmatkcjnvq76c3vn3ww3hjg"){
   token <- t
   return( paste("Bearer", token) )
 }
@@ -64,7 +30,7 @@ getApi <- function(apCode = "YVR", ac = "LH", loc = "", cab=""){
   url_airline_api_airport <- paste0(url_airline_api, ac)
   
   url_lounge_api <- "https://api.lufthansa.com/v1/offers/lounges/"
-  url_lounge_api_airport <- paste0(url_lounge_api, loc,"/",cab)
+  url_lounge_api_airport <- paste0(url_lounge_api, loc,"?cabinClass=",cab)
   
   # Getting the content from the Airport Resources API
   received_content1 <- httr::GET(url = url_airport_api_airport,
@@ -75,7 +41,13 @@ getApi <- function(apCode = "YVR", ac = "LH", loc = "", cab=""){
                                  config = httr::add_headers(Authorization = Authorization()))
   
   if (received_content1$status_code != 200) {
-    stop("Problem with calling the API - response: ", content(received_content1))
+    stop("Problem with calling the airport API - response: ", content(received_content1))
+  }
+  if (received_content2$status_code != 200) {
+    stop("Problem with calling the airline API - response: ", content(received_content1))
+  }
+  if (received_content3$status_code != 200) {
+    stop("Problem with calling the lounge API - response: ", content(received_content1))
   }
   
   airport_content <- httr::content(received_content1, "text",encoding = "UTF-8")
